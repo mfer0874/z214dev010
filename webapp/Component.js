ce3061d04bddc7483f2abff9125e51c070c4ebb2
mfer0874 <m.fer0874@gmail.com>
2024-01-27 19:17:27 -0300

    Alteracoes 27 Jan Estrutura Inicial

--- webapp/Component.js
+++ webapp/Component.js
@@ -11,9 +11,18 @@ sap.ui.define([
         init: function() {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
-            
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");    
+            // create the view based on the url/hash
             this.getRouter().initialize();
 
+        },
+        createContent: function() {
+            // create root view
+            return View({
+                viewName: "dev.z214dev010.view.App",
+                type: "XML"
+            });
         }
     });
 });
